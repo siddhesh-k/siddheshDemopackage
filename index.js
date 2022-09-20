73828cbe58edb8b623e29c8bacddbe170971addc
siddhesh khadapkar <siddhkhadapkar22@gmail.com>
2022-09-20 21:32:13 +0530

    hello

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 function shadow(options){
-    let images = document.querySelectorAll('.shadowizord');
-    if(options.shadow_type === 'hard'){
-        options.shadow_type = '0px'
-    }
-    else{
-        options.shadow_type = '15px'
-    }
-    images.forEach(image =>{
-        image.style.boxShadow = `10px 10px ${options.shadow_type}`
-    
-        if(options.padding){
-            image.style.padding= '1em'
-        }
-    })
+    console.log("hello world")
 }
-module.exports.shadow = shadow;
+module.exports = {shadow};
